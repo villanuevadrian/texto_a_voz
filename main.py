5f63a5ff753ce5826f8e45b5c09dec8f33ad7fe4
villanuevadrian <villanuevadrian88@gmail.com>
2023-09-28 10:58:49 +0200

    1.0

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 import nltk
+from newspaper import Article
+from gtts import gTTS
+from tempfile import TemporaryFile
+
+# Descargamos los recursos lingüistícos para el correcto funcionamiento del programa
 nltk.download()
-x = 0
-print(x)
+
+# Solicitamos al usuario que introduzca la URL del artículo para descargar el texto
+url = input("Introduce la url del artículo desesado: ")
+
+# Creamos el objeto de la clase Article y lo descargamos
+article = Article(url)
+article.download()
+
+# Analizamos la sintaxis
+article.parse()
+
+# Podemos mostrar por pantalla el texto para comprobar que el archivo mp3 se ha creado correctamente
+print(article.text)
+
+# Convierto el texto en un archivo mp3
+tts= gTTS(article.text,lang="es")
+
+# Creamos un objeto de la clase BytesIO que nos leera el archivo mp3
+mp3_fp = TemporaryFile()
+tts.write_to_fp(mp3_fp)
+
+# Guardamos el archivo mp3
+tts.save("audio.mp3")
